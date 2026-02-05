9ecddb8841cf92c13ea69aaaa1934b5b730d8056
AlendaSIA <alenda.jurmala@gmail.com>
2026-02-05 13:49:56 +0200

    Update __init__.py

--- steps/__init__.py
+++ steps/__init__.py
@@ -3,3 +3,4 @@ from . import step_01_fetch_sales_list
 from . import step_02_pick_next_doc
 from . import step_03_fetch_full_document
 from . import step_04_extract_client_data
+from . import step_05_pipedrive_ping
